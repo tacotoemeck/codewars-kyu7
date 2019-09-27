b23e4f851e11b26bb53eec8fba3596a6f8750cc0
Tomek Wagner <53991292+tacotoemeck@users.noreply.github.com>
2019-09-27 22:17:05 +0100

    Add files via upload

---
+++ findTheNExtPerfectSquare.js
@@ -0,0 +1,29 @@
+// You might know some pretty large perfect squares. But what about the NEXT one?
+
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+// If the parameter is itself not a perfect square, than -1 should be returned. You may assume the parameter is positive.
+
+// Examples:
+
+// findNextSquare(121) --> returns 144
+// findNextSquare(625) --> returns 676
+// findNextSquare(114) --> returns -1 since 114 is not a perfect
+
+function findNextSquare(n) {
+	let base = Math.sqrt(n)
+	let pow = 0;
+
+	if (!Number.isInteger(base)) {
+		return -1
+	}
+	else {
+		do { 
+			base++
+			pow = base**2 
+		} while (!Number.isInteger(pow))
+	}
+	return pow
+}
+
+findNextSquare(121)
