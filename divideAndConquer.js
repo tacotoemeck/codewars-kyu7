942c64509bdbc379ca3804e15a3f71ad0ae9b815
Tomek Wagner <53991292+tacotoemeck@users.noreply.github.com>
2019-12-11 09:18:17 +0000

    Create divideAndConquer.js

---
+++ divideAndConquer.js
@@ -0,0 +1,10 @@
+// Given a mixed array of number and string representations of integers, add up the string integers and subtract this from the total of the non-string integers.
+
+// Return as a number.
+
+function divCon(x){
+
+let numbersTotal = x.filter(number => typeof number == "number").reduce((a,b)=> a+b);
+let stringsTotal = x.filter(string => typeof string == "string" ).reduce((a,b) => Number(a) + Number(b));
+return numbersTotal - stringsTotal;
+}
